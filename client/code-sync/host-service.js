b6b169d2e6a86ece38420f101a9d1dbe871f5541
borrel <ifa.brainpower@gmail.com>
2021-07-18 22:42:58 +0000

    type in socket address var

--- client/code-sync/host-service.js
+++ client/code-sync/host-service.js
@@ -2,7 +2,7 @@ const host = {};
 host.addr = window._serverAddr || 'localhost:4000';
 host.protoHint = window._protoHint || '';
 
-host.webSocket = window._socketAddr || `ws${host.protoHint}://${host.addr}/notify`
+host.socketAddr = window._socketAddr || `ws${host.protoHint}://${host.addr}/notify`
 host.codeUrl = window._codeUrl || `http${host.protoHint}://${host.addr}/code-main`
 
 module.exports = host;
